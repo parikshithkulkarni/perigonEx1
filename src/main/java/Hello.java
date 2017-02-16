ae019ff9b4af99a1378e1f1e4e571f992c6cb3fb
parikshithkulkarni <parikshith.kulkarni@gmail.com>
2017-02-16 16:59:48 -0500

    My first commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Parikshith Kulkarni on 2/15/2017.
+ */
+public class Hello {
+
+    public static void main(String [] args){
+        System.out.println("hello world!");
+    }
+}
